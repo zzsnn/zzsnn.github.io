3404ce8bf0c9a10248969b1f2b228f56cf2b6506
TheFine <768463945@qq.com>
2020-06-15 18:39:38 +0800

    更新

--- MLJS/index .php
+++ MLJS/index .php
@@ -3,13 +3,13 @@
 <head>
 <meta charset="utf-8">
   <!--网页名称-->
-<title>欢迎光临 决裁者解析 - 免费观影 从这里开始</title>
+<title>欢迎光临 FOME影视 - 免费观影 从这里开始</title>
 <meta name="baidu-site-verification" content="b9lOcChmbf">
 <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
   <!--关键词-->
- <meta name="keywords" content="决裁者解析">
+ <meta name="keywords" content="FOME影视">
  <!--描述-->
- <meta name="description" content="决裁者解析">
+ <meta name="description" content="FOME影视">
  <!--浏览器小图标，如有，请上传-->
 <link rel="shortcut icon" href="1.ico">
 <link rel="stylesheet" href="style/css/main.css">
@@ -30,9 +30,9 @@
 	<div class="content">
 		<div  class="inner" >
           <!--标题-->
-			<h1><a >决裁者解析</a></h1>
+			<h1><a >FOME影视</a></h1>
 			<p>
-				<a >欢迎光临 决裁者解析 - 免费观影 从这里开始</a><br>
+				<a >欢迎光临 FOME影视 - 免费CDN加速观影 从这里开始</a><br>
 			</p>
 			<div >
 				<input style="width:55%;float:left;" type="text" id="href" placeholder="在这输入视频地址" size="22">
@@ -40,6 +40,7 @@
 <script>
 function local(){
 location.href = "https://v.nmbaojie.com/mingri/mingri.php?url=" + document.getElementById("href").value
+location.href = "https://ruodanglaishi.xyz/?url=" + document.getElementById("href").value
 }</script>
 			</div>
 		</div>
@@ -47,20 +48,22 @@ location.href = "https://v.nmbaojie.com/mingri/mingri.php?url=" + document.getEl
 	<nav>
 	<ul>
       <!--菜单-->
-               <li><a href="https://www.ipapp.vip">苹果QQ多开
-<li><a href="https://v.qq.com/" target="_blank">腾讯视频<li><a href="https://www.iqiyi.com/" target="_blank">爱奇艺视频<li><a href="https://www.youku.com/" target="_blank">优酷视频<li><a href="https://www.mgtv.com/" target="_blank">芒果视频 <li><a href="http://www.yunhu111.cn/" target="_blank">决裁者云呼
+			   <li><a href="https://v.qq.com/" target="_blank">腾讯视频
+				<li><a href="https://www.iqiyi.com/" target="_blank">爱奇艺视频
+					<li><a href="https://www.youku.com/" target="_blank">优酷视频
+						<li><a href="https://www.mgtv.com/" target="_blank">芒果视频
 </a>  
 	</li></ul>
 	
-<!--决裁者团队-QQ3336271687-->
+<!--折纸少年-QQ768463945-->
 <script src='https://js.qsk666.com/5/3030/8503/20/27.js'></script>
-<!--决裁者团队-QQ3336271687-->
+<!--折纸少年-QQ768463945-->
 
 	</nav></header>
 	
 					<footer id="footer">
-						<p class="copyright">Copyright © 2020-2060  <a href="/" target="_blank">决裁者工作室</a>版权所有</p>
-                                                <p class="copyright">本站由<a href="/">决裁者解析</a>提供技术支持</p>
+						<p class="copyright">Copyright © 2020-2060  <a href="https://zzsnn.github.io/" target="_blank">折纸少年</a>版权所有</p>
+                                                <p class="copyright">本站由<a href="https://github.com/zzsnn">Github</a>提供支持</p>
 			</div>
 	<script type="text/javascript" language="javascript">(function(){function k(a,b,c){if(a.addEventListener)a.addEventListener(b,c,false);else a.attachEvent&&a.attachEvent("on"+b,c)}function g(a){if(typeof window.onload!="function")window.onload=a;else{var b=window.onload;window.onload=function(){b();a()}}}function h(){var a={};for(type in{Top:"",Left:""}){var b=type=="Top"?"Y":"X";if(typeof window["page"+b+"Offset"]!="undefined")a[type.toLowerCase()]=window["page"+b+"Offset"];else{b=document.documentElement.clientHeight?document.documentElement:document.body; a[type.toLowerCase()]=b["scroll"+type]}}return a}function l(){var a=document.body,b;if(window.innerHeight)b=window.innerHeight;else if(a.parentElement.clientHeight)b=a.parentElement.clientHeight;else if(a&&a.clientHeight)b=a.clientHeight;return b}function i(a){this.parent=document.body;this.createEl(this.parent,a);this.size=Math.random()*5+5;this.el.style.width=Math.round(this.size)+"px";this.el.style.height=Math.round(this.size)+"px";this.maxLeft=document.body.offsetWidth-this.size;this.maxTop=document.body.offsetHeight- this.size;this.left=Math.random()*this.maxLeft;this.top=h().top+1;this.angle=1.4+0.2*Math.random();this.minAngle=1.4;this.maxAngle=1.6;this.angleDelta=0.01*Math.random();this.speed=2+Math.random()}var j=false;g(function(){j=true});var f=true;window.createSnow=function(a,b){if(j){var c=[],m=setInterval(function(){f&&b>c.length&&Math.random()<b*0.0025&&c.push(new i(a));!f&&!c.length&&clearInterval(m);for(var e=h().top,n=l(),d=c.length-1;d>=0;d--)if(c[d])if(c[d].top<e||c[d].top+c[d].size+1>e+n){c[d].remove(); c[d]=null;c.splice(d,1)}else{c[d].move();c[d].draw()}},40);k(window,"scroll",function(){for(var e=c.length-1;e>=0;e--)c[e].draw()})}else g(function(){createSnow(a,b)})};window.removeSnow=function(){f=false};i.prototype={createEl:function(a,b){this.el=document.createElement("img");this.el.setAttribute("src",b+"http://mimg.127.net/hxm/quan/hd/111207_sdj/style/img/snow.gif");this.el.style.position="absolute";this.el.style.display="block";this.el.style.zIndex="99999";this.parent.appendChild(this.el)},move:function(){if(this.angle< this.minAngle||this.angle>this.maxAngle)this.angleDelta=-this.angleDelta;this.angle+=this.angleDelta;this.left+=this.speed*Math.cos(this.angle*Math.PI);this.top-=this.speed*Math.sin(this.angle*Math.PI);if(this.left<0)this.left=this.maxLeft;else if(this.left>this.maxLeft)this.left=0},draw:function(){this.el.style.top=Math.round(this.top)+"px";this.el.style.left=Math.round(this.left)+"px"},remove:function(){this.parent.removeChild(this.el);this.parent=this.el=null}}})();createSnow("", 80); </script>
 	</footer>
