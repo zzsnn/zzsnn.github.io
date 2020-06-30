c92e19b0e0fe1c08d638238a2ddbf904862714ea
TheFine <768463945@qq.com>
2020-06-30 21:59:04 +0800

    更新

--- FHDL/js/first.js
+++
@@ -1,9 +0,0 @@
-mydate = localStorage.getItem('mydata')
-if (!mydate) {
-    $('.first').slideDown(800)
-    localStorage.setItem('mydata', 123)
-}
-$('.mianfeiban').click(function() {
-    $('.first').slideUp(800)
-
-})
