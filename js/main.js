66ee556385cd6b6ad521410bee36bb77c1197234
TheFine <768463945@qq.com>
2020-08-23 19:44:41 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -159,3 +159,93 @@
   });
 
 })(jQuery);
+
+// 淡入淡出
+var iUp = (function () {
+	var t = 0,
+		d = 150,
+		clean = function () {
+			t = 0;
+		},
+		up = function (e) {
+			setTimeout(function () {
+				$(e).addClass("up")
+			}, t);
+			t += d;
+		},
+		down = function (e) {
+			$(e).removeClass("up");
+		},
+		toggle = function (e) {
+			setTimeout(function () {
+				$(e).toggleClass("up")
+			}, t);
+			t += d;
+		};
+	return {
+		clean: clean,
+		up: up,
+		down: down,
+		toggle: toggle
+	}
+})();
+
+$(document).ready(function () {
+
+	// 获取一言数据
+	//fetch('https://v1.hitokoto.cn').then(function (res) {
+		//return res.json();
+	//}).then(function (e) {
+		//$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
+	//}).catch(function (err) {
+		//console.error(err);
+	//})
+
+	
+	// var url = 'https://query.yahooapis.com/v1/public/yql' + 
+    // '?q=' + encodeURIComponent('select * from json where url=@url') +
+    // '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8') +
+	// '&format=json&callback=?';
+
+	/**
+	 * 获取Bing壁纸
+	 * 原先 YQL 已经无法提供服务了
+	 * 改用 JsonBird：https://bird.ioliu.cn/
+	 * 
+	 */
+	var url = 'https://bird.ioliu.cn/v1/';
+	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
+	var index = sessionStorage.getItem("index");
+	var $header = $('#header');
+	if(imgUrls == null){
+		imgUrls = new Array();
+		index = 0;		
+		$.get(url,function (result) {
+			images = result.images;
+			for (let i = 0; i < images.length; i++) {
+				const item = images[i];
+				imgUrls.push(item.url);
+			}
+			var imgUrl = imgUrls[index];
+			var url = "https://www.bing.com"+imgUrl;
+			$header.css("background", "url('"+url+"') center center no-repeat #666");
+			$header.css("background-size", "cover");
+			sessionStorage.setItem("imgUrls",JSON.stringify(imgUrls));
+			sessionStorage.setItem("index",index);
+			});
+	}else{
+		if(index == 7)
+			index = 0;
+		else
+			index++;
+		var imgUrl = imgUrls[index];
+		var url = "https://www.bing.com"+imgUrl;
+		$header.css("background", "url('"+url+"') center center no-repeat #666");
+		$header.css("background-size", "cover");
+		sessionStorage.setItem("index",index);
+	}
+	
+	$(".iUp").each(function (i, e) {
+		iUp.up(e);
+	});
+});
