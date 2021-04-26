abd7a5717169c34993a878cd3951a22d5b8b8e46
zzsnn <62317652+zzsnn@users.noreply.github.com>
2021-04-26 09:37:03 +0800

    Update fqkk_auto_read.js

--- fqkk_auto_read.js
+++ fqkk_auto_read.js
@@ -55,13 +55,13 @@ const $ = new Env(`前台自动阅读`);
       if ($.isSurge() || $.isLoon()) {
         $.done({response: {status: 200, headers, body}})
       } else if ($.isQuanX()) {
-        $.done({status: 'HTTP/1.1 200 OK', headers, body})
+        $.done({status: 'HTTP/1.2.2 200 OK', headers, body})
       }
     } else if (typeof $response !== "undefined") {
       // 如果重定向的是微信文章，改写重定向地址
       let url302 = ($response.headers && $response.headers['Location']) || ''
-      if (url302.match(/https?:\/\/mp.weixin.qq.com\/s/)) {
-        $response.headers['Location'] = $request.url.replace('/task/read', 'https?:\/\/mp.weixin.qq.com\/s/mock/read')
+      if (url302.match(/https?:\/\/mp\.weixin\.qq\.com\/s/)) {
+        $response.headers['Location'] = $request.url.replace('https?:\/\/mp.weixin.qq.com\/s/mock/read/mock/read')
         $.done({headers: $response.headers})
       } else {
         $.log(`未检查到待跳转的微信文章url：\n${JSON.stringify($response.headers, null, 2)}`)
